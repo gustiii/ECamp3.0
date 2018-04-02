6ecf52b81054d22c78dafcfd95cb31e6a5580390
Philipp Fehr <calvin.philipp@gmail.com>
2018-04-02 13:07:53 +0200

    graphql base install

---
+++ ecamp3.0-graphql/server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+var { makeExecutableSchema } = require('graphql-tools');
+
+var typeDefs = [`
+type Query {
+	hello: String,
+	tschau: String
+}
+
+schema {
+  query: Query
+}`];
+
+var resolvers = {
+	Query: {
+		hello(root) {
+			return 'world';
+		},
+		tschau(root) {
+			return 'sepp';
+		}
+	}
+};
+
+var schema = makeExecutableSchema({typeDefs, resolvers});
+var app = express();
+app.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
+app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphiql'));
